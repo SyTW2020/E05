520cef115471132cd838dc29e05cc7bb95fe0771
Pablo Molina Martinez <alu0101103181@ull.edu.es>
2021-06-14 21:33:55 +0100

    Probando con firefox

--- DateMaker/e2e/protractor.conf.js
+++ DateMaker/e2e/protractor.conf.js
@@ -5,7 +5,7 @@ exports.config = {
   allScriptsTimeout: 11000,
   specs: ['./src/features/**/*.feature'],
   capabilities: {
-    browserName: 'chrome'
+    browserName: 'firefox'
   },
   directConnect: true,
   SELENIUM_PROMISE_MANAGER: false,
