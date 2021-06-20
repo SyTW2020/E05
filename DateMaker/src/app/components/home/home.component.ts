74fc5e21787a20f578e279d04c70b36512af736a
Maria <alu0101029801@ull.edu.es>
2021-06-20 20:39:48 +0100

    Creada Interfaz Course y añadiendo partes al home

--- DateMaker/src/app/components/home/home.component.ts
+++ DateMaker/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  courses : Course[] = []
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addCourse() {
+    
+  }
 }
