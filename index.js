f6ab11dafa020b4baa697a08b9bc97b9311d9a86
alu0101105741 <alu0101105741@ull.edu.es>
2021-06-23 20:17:51 +0100

    testing

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.static(process.cwd()+"./public"));
 app.use(express.json());
 
 app.use('/api', require('./routes/index'));
-
-app.listen(3000);
-console.log('Server listening on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
